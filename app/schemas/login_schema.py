36804bdef251b8d6ec25f02f3c0cd4bd5f549087
ByronSerrano <Bsantyserrano@gmail.com>
2024-07-25 21:54:27 -0500

    Changed User for Login

--- app/schemas/login_schema.py
+++ app/schemas/login_schema.py
@@ -1,13 +1,13 @@
 from marshmallow import fields
 from app.schemas.base_schema import BaseSchema  # Asegúrate de importar correctamente tu BaseSchema
 
-class UserSchema(BaseSchema):  # Define una nueva clase llamada UserSchema que hereda de BaseSchema
-    user_id = fields.Int(dump_only=True)  # Define un campo llamado "user_id" que se espera que sea un entero (Int) y solo se serializa (dump_only=True)
+class LoginSchema(BaseSchema):  # Define una nueva clase llamada UserSchema que hereda de BaseSchema
+    login_id = fields.Int(dump_only=True)  # Define un campo llamado "login_id" que se espera que sea un entero (Int) y solo se serializa (dump_only=True)
     identification = fields.Str(required=True, unique=True)  # Define un campo llamado "identification" que se espera que sea una cadena (Str) y es obligatorio (required=True)
     name = fields.Str(required=True)  # Define un campo llamado "name" que se espera que sea una cadena (Str) y es obligatorio (required=True)
     password = fields.Str(required=True)  # Define un campo llamado "password" que se espera que sea una cadena (Str) y es obligatorio (required=True)
     lastname = fields.Str(required=True)  # Define un campo llamado "lastname" que se espera que sea una cadena (Str) y es obligatorio (required=True)
     status = fields.Bool(dump_only=True)  # Define un campo llamado "status" que se espera que sea un booleano (Bool) y solo se serializa (dump_only=True)
     created_at = fields.DateTime(dump_only=True, format='%Y-%m-%d %H:%M:%S%z')  # Define un campo llamado "created_at" que se espera que sea una fecha y hora (DateTime) y solo se serializa (dump_only=True)
-user_schema = UserSchema()
-print(user_schema)  # Verificar esquema de serialización
+login_schema = LoginSchema()
+print(login_schema)  # Verificar esquema de serialización
--- app/services/login_service.py
+++ app/services/login_service.py
@@ -1,9 +1,9 @@
 import traceback
 from sqlalchemy import text
 from app.extensions import db, bcrypt_instance
-from app.models.user import User
+from app.models.login import Login
 from app.utils.utilities import timeNowTZ
-from app.schemas.user_schema import UserSchema
+from app.schemas.login_schema import LoginSchema
 from app.utils.error_handler import handle_error
 
 
@@ -16,39 +16,42 @@ def get_all():
     """
     try:
         # Consulta todos los usuarios activos en la base de datos
-        user_objects = db.session.query(User).filter(User.status == True).all()
-        print("Usuarios recuperados de la base de datos:", user_objects)  # Verifica los usuarios recuperados
+        login_objects = db.session.query(Login).filter(Login.status == True).all()
+        print("Usuarios recuperados de la base de datos:", login_objects)  # Verifica los usuarios recuperados
         # Serializa los objetos de usuario en una lista de diccionarios utilizando el esquema de usuario (UserSchema)
-        user_list = UserSchema(many=True).dump(user_objects)
-        print("Lista de usuarios serializados:", user_list)  # Verifica la lista de usuarios serializados
-        return user_list  # Devuelve la lista de usuarios serializados
+        login_list = LoginSchema(many=True).dump(login_objects)
+        print("Lista de usuarios serializados:", login_list)  # Verifica la lista de usuarios serializados
+        return login_list  # Devuelve la lista de usuarios serializados
     except Exception as e:  # Captura cualquier excepción que ocurra durante la ejecución del bloque try
         print(f"Error al obtener usuarios: {str(e)}")  # Imprime el mensaje de error
         traceback.print_exc()  # Imprimir la traza completa de la excepción
         return None  # Devuelve None para indicar que ocurrió un error durante la operación
 
 def exists(identification: str):
-    user_objects = (
-        db.session.query(User)
-        .filter(User.identification == identification, User.status == True)
+    login_objects = (
+        db.session.query(Login)
+        .filter(Login.identification == identification, Login.status == True)
         .first()
     )
-    return user_objects is not None
+    return login_objects is not None
 
-def create( identification: str, name: str, lastname: str, password: str):
-    try :        
-        new_user = User(
+def create( identification: str, name: str, lastname: str, password: str = None):
+    try:
+        if password is None:
+            password = identification 
+                
+        new_login = Login(
             identification = identification,
             name=name,
             lastname=lastname,
             password=bcrypt_instance.generate_password_hash(password).decode("utf8"),
             status=True
         )
-        db.session.add(new_user)
+        db.session.add(new_login)
 
         db.session.commit()
-        user_list = UserSchema(exclude=["password"]).dump(new_user)
-        return user_list
+        login_list = LoginSchema(exclude=["password"]).dump(new_login)
+        return login_list
 
     except Exception as e:
         print(f"Error al crear usuario: {str(e)}")
